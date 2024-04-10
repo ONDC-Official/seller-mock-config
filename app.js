54a219ce4185f57eb83a007ee1da811e6ee410d7
Mohd Ahsan <63741546+mofahsan@users.noreply.github.com>
2024-04-10 05:40:28 +0530

    mobility seller backend config added

---
+++ app.js
@@ -0,0 +1,28 @@
+const $RefParser = require("json-schema-ref-parser");
+const yaml = require("js-yaml");
+const fs = require("fs")
+var uiPath = "./build/build.js";
+var yamlOutput = "./build/build.yaml";
+
+
+const indexYamlPath = './configs/mobility/mobility.yaml'
+
+async function baseYMLFile(file) {
+    try {
+      const schema = await $RefParser.dereference(file);
+      return schema;
+    } catch (error) {
+      console.error("Error parsing schema:", error);
+    }
+  }
+
+  baseYMLFile(indexYamlPath).then((res)=>{
+    const jsonDump = "let build_spec = " + JSON.stringify(res);
+    fs.writeFileSync(uiPath, jsonDump, "utf8");
+
+    const output = yaml.dump(res);
+    fs.writeFileSync(yamlOutput, output, "utf8");
+  }).catch((err)=>{
+     console.log(err)
+  })
+
