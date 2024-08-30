5bff4b945e26acc75b64480de5e4c89d4cc59635
Mohd Ahsan <63741546+mofahsan@users.noreply.github.com>
2024-08-30 18:50:06 +0530

    init master v2

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# seller-mock-config
+# Introduction
+
+The Seller mock engine config converts yaml configs into single build.json file that can be consumed by the seller mock engine through the api.
+
+By leveraging seller-mock-config, seller mock engine can pull latest config files without need to update configs. It ensures that the seller mock engine is running with latest configs.
+
+# Repository Structure
+
+- `/config` - stores configs in yamls
+- `/build`
+  - `build.json`  - build output
+- `app.js`  - code to convert yamls into build.js
+
+
+# Steps to setup protocol servel in local environment
+
+## Prerequisites
+
+- Install node (https://nodejs.org/en)
+
+## Steps to setup the local environment
+
+Go to project root and install dependencies
+```
+npm i
+```
+
+make config changes and run 
+```
+node app
+```
+
+This will output config from config folder into build/build.json
+which can be retrieved using github/other api
+
+
---
+++ app.js
@@ -0,0 +1,50 @@
+  const $RefParser = require("json-schema-ref-parser");
+  const yaml = require("js-yaml");
+  const fs = require("fs")
+  var uiPath = "./build/build.js";
+  var uiPathJson = "./build/build.json";
+  
+  const outputFolderPath = "./build";
+  
+  
+  const indexYamlPath = './configs/main.yaml'
+  
+  
+  // read yaml
+  async function baseYMLFile(file) { 
+      try {
+        const schema = await $RefParser.dereference(file);
+        return schema;
+      } catch (error) {
+        console.error("Error parsing schema:", error);
+      }
+    }
+  
+  
+  
+    function outputBuild(outputPath,flow){
+        fs.writeFileSync(`${outputFolderPath}/${outputPath}`, JSON.stringify(flow), "utf8");
+    }
+  
+    // driver 
+    (async function driver(){
+      const args = process.argv[2]
+      let config = await baseYMLFile(indexYamlPath)
+      config = config?.configs
+      if(!args){ // run all flows
+        for (const flow of config){
+          outputBuild(flow.filename,flow.path)
+        }
+      }else{ // run only specific flow
+        const flow = config.find((element)=>args===element.domain)
+        outputBuild(flow.filename,flow.path)
+      }
+      
+    })()
+  
+  
+  
+  
+  
+  
+  
